5d27ffc2acacdcae0242f40514df1db29afbc1eb
ken.hurtado <khurtado@protonmail.com>
2019-02-04 13:09:25 -0700

    Added image for webpack config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import pic from './img/me.jpg';
 
-const App = () => <h1>Hello, World!</h1>;
+const App = () => <h1>Hello, World! <img src={pic} /></h1> ;
 
 export default App;
