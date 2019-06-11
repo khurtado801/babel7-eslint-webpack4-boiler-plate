e3226d17647a4bffc2620198a80a3051886b86df
ken.hurtado <khurtado@protonmail.com>
2019-06-11 11:16:31 -0600

    installed react-bootstrap and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import pic from './img/me.jpg';
 
-const App = () => <h1>Hello, World! <img src={pic} alt="image"/></h1> ;
+const App = () => <h1><img src={pic} alt="image"/>Hello, World!</h1> ;
 
 export default App;
